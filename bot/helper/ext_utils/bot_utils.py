06789cc4d994fb2d81cff566420dc0ea3e2b7fdd
Rajveer0916 <94645722+Rajveer0916@users.noreply.github.com>
2022-12-01 00:55:44 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -313,15 +313,15 @@ def get_readable_message():
                 buttons.sbutton("⏪Previous", "status pre")
                 buttons.sbutton(f"{PAGE_NO}/{PAGES}", str(THREE))
                 buttons.sbutton("Next⏩", "status nex")
-                buttons.sbutton("Refresh", "status refresh")
                 buttons.sbutton("Statistics", str(THREE))
+                buttons.sbutton("Refresh", "status refresh")
                 buttons.sbutton("Close", "status close")
             else:
                 buttons.sbutton("Previous", "status pre")
                 buttons.sbutton(f"{PAGE_NO}/{PAGES}", str(THREE))
                 buttons.sbutton("Next", "status nex")
-                buttons.sbutton("Refresh", "status refresh")
                 buttons.sbutton("Statistics", str(THREE))
+                buttons.sbutton("Refresh", "status refresh")
                 buttons.sbutton("Close", "status close")
             button = buttons.build_menu(3)
             return msg + bmsg, button
