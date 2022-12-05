8b5ab2453cdd02c76729e251c8e113d0000ffdfd
Rajveer0916 <94645722+Rajveer0916@users.noreply.github.com>
2022-12-06 01:00:58 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -507,6 +507,7 @@ Tasks: {tasks}
 CPU: {progress_bar(cpu)} {cpu}%
 RAM: {progress_bar(mem)} {mem}%
 DISK: {progress_bar(disk)} {disk}%
+
 SENT: {sent} | RECV: {recv}
 DLs: {num_active} | ULs: {num_upload} | SEEDING: {num_seeding}
 ZIP: {num_zip} | UNZIP: {num_unzip} | SPLIT: {num_split}
