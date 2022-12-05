c9c1698da18339476d14dbbad85d72938f790735
Rajveer0916 <94645722+Rajveer0916@users.noreply.github.com>
2022-12-06 00:53:54 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -505,13 +505,11 @@ def bot_sys_stats():
 Made with ❤️ by {config_dict['CREDIT_NAME']}
 
 Tasks: {tasks}
-
 CPU: {progress_bar(cpu)} {cpu}%
 RAM: {progress_bar(mem)} {mem}%
 DISK: {progress_bar(disk)} {disk}%
 
 SENT: {sent} | RECV: {recv}
-
 DLs: {num_active} | ULs: {num_upload} | SEEDING: {num_seeding}
 ZIP: {num_zip} | UNZIP: {num_unzip} | SPLIT: {num_split}
 """
